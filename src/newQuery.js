46ced04b590700e58ca04a58da38f715961b0418
newcaptain <newcaptain@foxmail.com>
2019-07-04 18:29:50 +0800

    update 增加修改样式和hide、show操作

--- src/newQuery.js
+++ src/newQuery.js
@@ -8,7 +8,6 @@
 
 'use strict';
 
-// 封装变量，防止污染全局变量
 (function(window, undefined) {
   
   var rHtml = /^\s*<[\w\W]+>/;
@@ -60,10 +59,14 @@
         if (match) {
           doms = buildFragment(match[0]);
 
-          // 处理查询
+        // 处理查询
         } else {
+
+          // 带context查询
           if (!context) {
             doms = document.querySelectorAll(selector);
+
+          // 不带context查询
           } else if (typeof context === 'newQuery') {
             context[0].querySelectorAll(selector);
           }
@@ -83,11 +86,95 @@
       if (typeof content === 'string') {
         var doms = buildFragment(content);
         for (var i=0; i<this.length; i++) {
-          this[i].appendChild(doms[0]);
+          for (var j=0; j<doms.length; j++) {
+            this[i].appendChild(doms[j]);
+          }
         }
       }
-    }
+    },
 
+    after: function(content) {
+      if (typeof content === 'string') {
+        var doms = buildFragment(content);
+        for (var i=0; i<this.length; i++) {
+          for (var j=0; j<doms.length; j++) {
+            this[i].parentNode.insertBefore(doms[j], this[i].nextSibling);
+          }
+        }
+      }
+    },
+    
+    prepend: function(content) {
+      if (typeof content === 'string') {
+        var doms = buildFragment(content);
+        for (var i=0; i<this.length; i++) {
+          for (var j=0; j<doms.length; j++) {
+            this[i].insertBefore(doms[j], this[i].firstChild);
+          }
+        }
+      }
+    }, 
+
+    before: function(content) {
+      if (typeof content === 'string') {
+        var doms = buildFragment(content);
+        for (var i=0; i<this.length; i++) {
+          for (var j=0; j<doms.length; j++) {
+            this[i].parentNode.insertBefore(doms[j], this[i]);
+          }
+        }
+      }
+    },
+
+    css: function(property, value) {
+      // 展示css
+      if (arguments.length < 2) {
+        var element = this[0];
+        if (typeof property === 'string') {
+          if (!element) return;
+          return element.style[property];
+        } else if (likeArray(property)) {
+          if (!element) return;
+          var props = {},
+              computedStyle = getComputedStyle(element, '');
+          for (var prop of property) {
+            props[prop] = computedStyle[prop]
+          }
+          return props;
+        }
+      }
+
+      // 设置css
+      var css = '';
+      if (typeof property === 'string') {
+        if (!value && value !== 0) {
+          each(this, function(item) {
+            item.style.removeProperty(property);
+          });
+        } else {
+          css = property + ":" + value;
+        }
+      } else {
+        for (var key in property) {
+          if (!property[key] && property[key] !== 0) {
+            each(this, function(item) {
+              item.style.removeProperty(key);
+            })
+          } else {
+            css += key + ":" + property[key] + ";";
+          }
+        }
+      }
+      return each(this, function(item) { item.style.cssText += ';' + css})
+    },
+
+    hide: function() {
+      this.css('display', 'none');
+    },
+
+    show: function() {
+      this.css('display', 'block');
+    }
   }
 
   newQuery.fn.init.prototype = newQuery.fn;
